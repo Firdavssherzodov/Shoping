6a037c77d8e186ca865d81809c414fd88888568f
firdavs <frontendhtml1234@gmail.com>
2023-12-06 20:39:07 +0500

    yangiliklar makoni

--- src/components/Bolimlar.vue
+++ src/components/Bolimlar.vue
@@ -28,7 +28,6 @@ onMounted(() => {
     }
     // Elektronika
     if (value == "Elektronika") {
-      ProductsList.srt = [];
       let Elektronika = ProductsList.srt.sort((first, second) => {
         if (first.category < second.category) return -1;
         if (first.category > second.category) return 1;
@@ -38,8 +37,6 @@ onMounted(() => {
     }
     // Sumkalar
     if (value == "Sumkalar") {
-      ProductsList.srt = [];
-
       let Sumkalar = ProductsList.srt.sort((first, second) => {
         if (first.description <= second.description) return -1;
         if (first.description >= second.description) return 1;
@@ -49,8 +46,6 @@ onMounted(() => {
     }
     // NarxiB
     if (value == "NarxiB") {
-      ProductsList.srt = [];
-
       let NarxiB = ProductsList.srt.sort((first, second) => {
         if (first.price > second.price) return -1;
         if (first.price < second.price) return 1;
@@ -60,8 +55,6 @@ onMounted(() => {
     }
     // NarxiP
     if (value == "NarxiP") {
-      ProductsList.srt = [];
-
       let NarxiP = ProductsList.srt.sort((first, second) => {
         if (first.price < second.price) return -1;
         if (first.price > second.price) return 1;
