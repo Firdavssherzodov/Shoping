f66a5b25002c0c6e6ed03170c48b07de447893b5
firdavs <frontendhtml1234@gmail.com>
2023-12-11 15:10:23 +0500

    yangilik

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <Header1 />
     <Search />
-    <karusel />
+    <karusel10/>
     <Bolim />
     <Maxsulot />
   </div>
@@ -13,7 +13,7 @@ import Header1 from "../components/Header.vue";
 import Search from "../components/Search.vue";
 import Bolim from "../components/Bolimlar.vue";
 import Maxsulot from "../components/Maxsulot.vue";
-import karusel from '../components/carusel.vue'
+import karusel10 from '../components/carusel.vue'
 </script>
 
 <style scoped></style>
