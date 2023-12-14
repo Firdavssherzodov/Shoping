f20c3c4e1831582f5751c8338981311e5c03f3a0
firdavs <frontendhtml1234@gmail.com>
2023-12-14 16:08:08 +0500

    yangnilik;ar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,7 +13,7 @@ import Header1 from "../components/Header.vue";
 import Search from "../components/Search.vue";
 import Bolim from "../components/Bolimlar.vue";
 import Maxsulot from "../components/Maxsulot.vue";
-import karusel from "../components/carusel.vue";
+import karusel from "../components/Karusel.vue";
 </script>
 
 <style scoped></style>
