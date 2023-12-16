1235224610a143fbb8a48f7aa9ab409e56f21153
firdavs <frontendhtml1234@gmail.com>
2023-12-16 10:56:29 +0500

    yandex44

--- src/components/Bolimlar.vue
+++ src/components/Bolimlar.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="w-full sm:py-5 py-1 ">
-    <ul class=" flex flex-wrap sm:justify-between justify-around sm:w-2/4 w-11/12  m-auto sm:ps-0 sm:text-lg text-[14.5px] sm:font-medium font-light">
-      <li @click="Sort('kiyim')" class="hover:text-blue-700 cursor-pointer">Kiyimlar</li>
-      <li @click="Sort('Sumkalar')" class="hover:text-blue-700 cursor-pointer">Sumkalar</li>
-      <li @click="Sort('Elektronika')" class="hover:text-blue-700 cursor-pointer">Elektronika</li>
-      <li @click="Sort('NarxiB')" class="hover:text-blue-700 cursor-pointer">Narxi B</li>
-      <li @click="Sort('NarxiP')" class="hover:text-blue-700 cursor-pointer">Narxi P</li>
-      <li @click="Sort('NarxiP')" class="hover:text-blue-700 cursor-pointer">Narxi P</li>
-      <li @click="Sort('NarxiP')" class="hover:text-blue-700 cursor-pointer">Narxi P</li>
-
-
+  <div class="w-full sm:py-5 py-1">
+    <ul
+      class="flex flex-wrap sm:justify-between justify-around sm:w-2/4 w-11/12 m-auto sm:ps-0 sm:text-lg text-[14.5px] sm:font-medium font-light"
+    >
+      <li @click="Sort('kiyim')" class="hover:text-blue-700 cursor-pointer">
+        Kiyimlar
+      </li>
+      <li @click="Sort('Sumkalar')" class="hover:text-blue-700 cursor-pointer">
+        Sumkalar
+      </li>
+      <li
+        @click="Sort('Elektronika')"
+        class="hover:text-blue-700 cursor-pointer"
+      >
+        Elektronika
+      </li>
+      <li @click="Sort('NarxiB')" class="hover:text-blue-700 cursor-pointer">
+        Narxi B
+      </li>
+      <li @click="Sort('NarxiP')" class="hover:text-blue-700 cursor-pointer">
+        Narxi P
+      </li>
+      <li @click="Sort('NarxiP')" class="hover:text-blue-700 cursor-pointer">
+        Narxi P
+      </li>
+      <li @click="Sort('NarxiP')" class="hover:text-blue-700 cursor-pointer">
+        Narxi P
+      </li>
     </ul>
   </div>
 </template>
@@ -63,10 +80,8 @@ function Sort(value) {
 </script>
 
 <style scoped>
-
-  ul>li{
-    padding: 0.4rem;
-    color:black;
-    
-    }
+ul > li {
+  padding: 0.4rem;
+  color: black;
+}
 </style>
