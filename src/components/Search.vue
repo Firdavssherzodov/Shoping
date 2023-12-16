fca440155530f03016a41575573fd7e76a1f5d84
firdavs <frontendhtml1234@gmail.com>
2023-12-16 10:51:13 +0500

    s

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,34 +1,33 @@
 <template>
-<div class="sm:w-2/4 w-11/12 m-auto sm:mt-4 mt-5">
-  <fwb-input
-    v-model="Search"
-    placeholder=" search query"
-    size="lg"
-    class="rounded-xl sm:py-3 py-[0.8rem] sm:my-[0.30rem] my-[0.28rem] border-color1 text-gray-500"
-  >
-    <template #prefix>
-      <svg
-        aria-hidden="true"
-        class="w-5 h-5 text-gray-500 dark:text-gray-400"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-        />
-      </svg>
-    </template>
-    <template #suffix>
-      <fwb-button >Search</fwb-button>
-    </template>
-  </fwb-input>
-
-</div>
+  <div class="sm:w-2/4 w-11/12 m-auto sm:mt-4 mt-5">
+    <fwb-input
+      v-model="Search"
+      placeholder=" search query"
+      size="lg"
+      class="rounded-xl sm:py-3 py-[0.8rem] sm:my-[0.30rem] my-[0.28rem] border-color1 text-gray-500"
+    >
+      <template #prefix>
+        <svg
+          aria-hidden="true"
+          class="w-5 h-5 text-gray-500 dark:text-gray-400"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          />
+        </svg>
+      </template>
+      <template #suffix>
+        <fwb-button>Search</fwb-button>
+      </template>
+    </fwb-input>
+  </div>
 </template>
 <!-- sm:px-3 px-2 py-3  border sm:w-3/4 m-uto w-[84%] px-2 -->
 <script setup>
@@ -52,9 +51,7 @@ watchEffect(() => {
 </script>
 
 <style scoped>
-
-:deep button{
+/* :deep button {
   margin-top: 3rem;
-}
-  
+} */
 </style>
