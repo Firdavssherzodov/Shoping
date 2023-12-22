ec840c23cf66c1ba172470d0c10784d0c5d996d6
firdavs <frontendhtml1234@gmail.com>
2023-12-22 17:38:02 +0500

    a

--- src/Admin/Admin.vue
+++ src/Admin/Admin.vue
@@ -5,7 +5,7 @@
     <p class="text-center font-sans text-xl py-5">Admin</p>
 
     <span
-      class="lg:w-[9%] border flex justify-between items-center py-4 rounded-xl cursor-pointer hover:bg-slate-200 absolute lg:left-[80%] left-[60%] md:bottom-1 bottom-"
+      class="lg:w-[9%] border flex justify-between items-center py-4 rounded-xl cursor-pointer hover:bg-slate-200 absolute md:left-[80%] left-[55%] md:bottom-1 bottom-"
     >
       <i class="px-1 pi pi-plus"></i>
       <p>Yangi mahsulot</p>
