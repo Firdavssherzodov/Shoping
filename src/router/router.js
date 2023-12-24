94560f534b06e1c16224971a48a3f3c4f626b8ff
firdavs <frontendhtml1234@gmail.com>
2023-12-24 11:02:04 +0500

    m

--- src/router/router.js
+++ src/router/router.js
@@ -14,7 +14,6 @@ const router = createRouter({
       name: "savat",
       component: () => import("../QoshimchaPage.vue/Korzinka.vue"),
     },
-    
     {
       path: "/user",
       name: "user",
